fec71a495a8275cc801a9a315bae0474e5a1a607
Nikhil Sharma <nikhilsharmash65@gmail.com>
2025-06-20 23:05:05 +0530

    intial repo structure and setup

---
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,31 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.User;
+import com.example.demo.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+@Tag(name = "User API", description = "Operations related to users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    @Operation(summary = "Get all users")
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @PostMapping
+    @Operation(summary = "Create a new user")
+    public User createUser(@RequestBody User user) {
+        return userService.createUser(user);
+    }
+}
